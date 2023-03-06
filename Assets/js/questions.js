51d363b61b1a1724795c3fcd75d5fce8718266a9
Ryan Golder <rgolder78@gmail.com>
2023-03-06 18:31:48 +1100

    Add extra js file for questions

---
+++ Assets/js/questions.js
@@ -0,0 +1,14 @@
+// Questions below yet to be linked into the html file
+const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "Rome", "Berlin", "Madrid"],
+      correctAnswer: "Paris"
+    },
+    {
+      question: "What is the highest mountain in the world?",
+      options: ["Mount Kilimanjaro", "Mount Everest", "Mount Fuji", "Mount McKinley"],
+      correctAnswer: "Mount Everest"
+    },
+    // Add more questions here...
+  ];
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,33 @@
+// Preliminary ideas for JS code.
+// Potentially have one html for highscores
+// One for logic, one for questions, and one for scores
+// do an if statement to see if user got answer wrong to decrement time
+// have a variable of the current question
+
+
 var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer-count");
+var timerInterval = null;
+
+// Countdown Function when start button is hit
+function countdown() {
+  if (timerInterval !== null) {
+    // A timer is already running, so don't start a new one
+    return;
+  }
+
+  var timeLeft = 5;
+
+  timerInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timerElement.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timerElement.textContent = '0';
+      clearInterval(timerInterval);
+      timerInterval = null; // Reset the timer interval variable
+    }
+  }, 1000);
+}
+
+startButton.addEventListener("click", countdown);
