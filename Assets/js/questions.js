8b9e500c234a7577789416310aae87f2ad0b3519
Ryan Golder <rgolder78@gmail.com>
2023-03-07 23:01:09 +1100

    updates to all files

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,14 +1,39 @@
 // Questions below yet to be linked into the html file
 const questions = [
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "Rome", "Berlin", "Madrid"],
-      correctAnswer: "Paris"
-    },
-    {
-      question: "What is the highest mountain in the world?",
-      options: ["Mount Kilimanjaro", "Mount Everest", "Mount Fuji", "Mount McKinley"],
-      correctAnswer: "Mount Everest"
-    },
-    // Add more questions here...
-  ];
+  {
+    question: "What is used to create a string?",
+    options: ["Full stops", "Commas", "Quotation marks", "Wool"],
+    correctAnswer: "Quotation marks",
+  },
+  {
+    question:
+      "How many days did it take Brandan Eich to develop the scripting language now known as JavaScript?",
+    options: ["486", "5016", "78", "10"],
+    correctAnswer: "10",
+  },
+  {
+    question: "What from the following isn't used in declaring a variable?",
+    options: ["const", "let", "var", "variable-declare"],
+    correctAnswer: "variable-declare",
+  },
+  {
+    question: "What does css stand for?",
+    options: [
+      "cascading style sheets",
+      "cold soothing sounds",
+      "cascading sheet styling",
+      "cool system scenes",
+    ],
+    correctAnswer: "cascading style sheets",
+  },
+  {
+    question: "Why do we code?",
+    options: [
+      "just cause...",
+      "cause i wana",
+      "to build an AI to take over the world",
+      "to turn an idea into a project",
+    ],
+    correctAnswer: "to turn an idea into a project",
+  },
+];
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,38 +4,9 @@
 // do an if statement to see if user got answer wrong to decrement time
 // have a variable of the current question
 
-var startButton = document.querySelector(".start-button");
-var timerElement = document.querySelector(".timer-count");
-var timerInterval = null;
-
-// Countdown Function when start button is hit
-function countdown() {
-  if (timerInterval !== null) {
-    // A timer is already running, so don't start a new one
-    return;
-  }
-
-  var timeLeft = 5;
-
-  timerInterval = setInterval(function () {
-    if (timeLeft > 0) {
-      timerElement.textContent = timeLeft;
-      timeLeft--;
-    } else {
-      timerElement.textContent = "0";
-      clearInterval(timerInterval);
-      timerInterval = null; // Reset the timer interval variable
-    }
-  }, 1000);
-}
-
-startButton.addEventListener("click", countdown);
-
 // var startButton = document.querySelector(".start-button");
 // var timerElement = document.querySelector(".timer-count");
 // var timerInterval = null;
-// var currentQuestion = 0;
-// var score = 0;
 
 // // Countdown Function when start button is hit
 // function countdown() {
@@ -44,73 +15,101 @@ startButton.addEventListener("click", countdown);
 //     return;
 //   }
 
-//   var timeLeft = 30;
+//   var timeLeft = 5;
 
 //   timerInterval = setInterval(function () {
 //     if (timeLeft > 0) {
 //       timerElement.textContent = timeLeft;
 //       timeLeft--;
 //     } else {
-//       timerElement.textContent = '0';
+//       timerElement.textContent = "0";
 //       clearInterval(timerInterval);
 //       timerInterval = null; // Reset the timer interval variable
-//       alert('Time\'s up!');
 //     }
 //   }, 1000);
 // }
 
-// // Show the current question
-// function showQuestion() {
-//   var questionText = document.querySelector(".question-text");
-//   var options = document.querySelectorAll(".option");
-
-//   questionText.textContent = questions[currentQuestion].question;
-
-//   for (var i = 0; i < options.length; i++) {
-//     options[i].textContent = questions[currentQuestion].options[i];
-//     options[i].addEventListener("click", checkAnswer);
-//   }
-// }
-
-// // Check the user's answer and update the timer if necessary
-// function checkAnswer() {
-//   if (this.textContent === questions[currentQuestion].correctAnswer) {
-//     alert('Correct!');
-//     score++;
-//   } else {
-//     alert('Incorrect!');
-//     var timeLeft = parseInt(timerElement.textContent);
-//     timeLeft -= 10;
-//     if (timeLeft < 0) {
-//       timeLeft = 0;
-//     }
-//     timerElement.textContent = timeLeft;
-//   }
-
-//   currentQuestion++;
-
-//   if (currentQuestion < questions.length) {
-//     showQuestion();
-//   } else {
-//     clearInterval(timerInterval);
-//     timerInterval = null; // Reset the timer interval variable
-//     alert('Your score is: ' + score);
-//     var initials = prompt('Enter your initials:');
-//     // Save the score and initials to local storage
-//     localStorage.setItem('highscore', score);
-//     localStorage.setItem('initials', initials);
-//   }
-// }
-
 // startButton.addEventListener("click", countdown);
-// showQuestion();
 
-// Note that in this updated code, we have added a currentQuestion variable to keep track of the current question being displayed, and a score variable to keep track of the user's score.
+var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer-count");
+var questionText = document.querySelector(".question-text");
+var options = document.querySelectorAll(".option-button");
+var showQuizCard = document.querySelector(".card-quiz");
+var timerInterval = null;
+var currentQuestion = 0;
+var score = 0;
 
-// We have also updated the countdown function to set the initial time to 30 seconds, and added an alert message to be displayed when the time runs out.
+// Displays the hidden question card
+function showSection() {
+  showQuizCard.style.display = 'block';
+}
 
-// The showQuestion function has been updated to populate the question and answer options from the questions array, and add an event listener to each option to check the user's answer.
+// Show the current question
+function showQuestion() {
+  questionText.textContent = questions[currentQuestion].question;
+  for (var i = 0; i < options.length; i++) {
+    options[i].textContent = questions[currentQuestion].options[i];
+  }
+  showSection();
+}
 
-// The checkAnswer function has been added to check the user's answer, update the score and timer, and move on to the next question when the user selects an answer. Once all questions have been answered, it displays the user's score, prompts them to enter their initials, and saves the score and initials to local storage.
+// Check the user's answer and update the timer if necessary
+function checkAnswer() {
+  if (this.textContent === questions[currentQuestion].correctAnswer) {
+    alert('Correct!');
+    score++;
+  } else {
+    alert('Incorrect!');
+    var timeLeft = parseInt(timerElement.textContent);
+    timeLeft -= 10;
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    }
+    timerElement.textContent = timeLeft;
+  }
 
-// Keep in mind that this is just one way to implement the functionality you described, and there are many other approaches you could take.
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    showQuestion();
+  } else {
+    clearInterval(timerInterval);
+    timerInterval = null; // Reset the timer interval variable
+    alert('Your score is: ' + score);
+    var initials = prompt('Enter your initials:');
+    // Save the score and initials to local storage
+    localStorage.setItem('highscore', score);
+    localStorage.setItem('initials', initials);
+  }
+}
+
+// Countdown Function when start button is hit
+function countdown() {
+  if (timerInterval !== null) {
+    // A timer is already running, so don't start a new one
+    return;
+  }
+
+  var timeLeft = 30;
+
+  timerInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timerElement.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timerElement.textContent = '0';
+      clearInterval(timerInterval);
+      timerInterval = null; // Reset the timer interval variable
+      alert('Time\'s up!');
+    }
+  }, 1000);
+  showQuestion();
+}
+
+// Add event listeners for the start button and options
+startButton.addEventListener("click", countdown);
+for (var i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", checkAnswer);
+}
+
